c6c088b3a45819f7bf76b4ecf7724f0fda03b9ba
Nourayn-said <saidnourayn@gmail.com>
2025-07-18 15:52:54 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "lucky~2ZljnQjK#3k5b02pvB7f_Uo-B4D1lwwpnGyAcmXodDiife2v-xu0",
 // add your Session Id make sure it starts with lucky~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -68,7 +68,7 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "255774351895",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "",
